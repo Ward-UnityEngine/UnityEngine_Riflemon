f5a91252f84363174a2610a42dce74c2508093e2
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-03-10 23:49:46 +0100

    Imported sprites + player movement

--- Riflemon/Assets/Scripts/Behaviour/PlayerBehaviour.cs
+++ Riflemon/Assets/Scripts/Behaviour/PlayerBehaviour.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    public RiflemonInput inputActions;
+    public float movementSpeed;
+    private InputAction move;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions = new RiflemonInput();
+        move = inputActions.Player.Move;
+        move.Enable();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 value = move.ReadValue<Vector2>();
+        rb.velocity = value.normalized * movementSpeed;
     }
 }
