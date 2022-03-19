7d5492ef8b472b5b93854d2133bde6832f5b1ddb
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-03-19 15:02:53 +0100

    Updated dialogbox behaviour
    Now we can display multiple lines of text after each other

--- Riflemon/Assets/Scripts/Overlay/DialogBorderBehaviour.cs
+++ Riflemon/Assets/Scripts/Overlay/DialogBorderBehaviour.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Text;
 
 public class DialogBorderBehaviour : MonoBehaviour
 {
     private PlayerBehaviour playerBehaviour;
-    string text = string.Empty;
+    private string[] text;
+    private int displayIndex = 0;
 
     private TextMeshProUGUI textUI;
+    private Animator blinkingArrowAnim;
     private Coroutine revealingCoroutine;
     private bool coroutineRunning;
     private OverlayStarter overlayStarter;
 
     public float textRevealInterval;
+    public float dramaticTextRevealInterval;
+    private float currentTextRevealInterval;
+    public int maxLenString;
 
     private void Awake()
     {
         textUI = GetComponentInChildren<TextMeshProUGUI>();
+        blinkingArrowAnim = GetComponentInChildren<Animator>();
+    }
+    private void Start()
+    {
+        blinkingArrowAnim.SetBool("Blinking", false);
     }
 
     private void Update()
@@ -27,24 +38,76 @@ public class DialogBorderBehaviour : MonoBehaviour
             if (coroutineRunning)
             {
                 StopCoroutine(revealingCoroutine);
+                blinkingArrowAnim.SetBool("Blinking", true);
                 coroutineRunning = false;
-                textUI.text = text;
+                textUI.text = text[displayIndex];
             }
             else
             {
-                end_dialog();
+                if(displayIndex>=(text.Length-1))
+                    end_dialog();
+                else
+                {
+                    //display next sentence
+                    displayIndex++;
+                    revealingCoroutine = StartCoroutine("revealText");
+                }
             }
         }
     }
 
-    public void start_dialog(PlayerBehaviour playerBehaviour,string targetText,OverlayStarter overlayStarter)
+    public void start_dialog(PlayerBehaviour playerBehaviour,string[] targetText,OverlayStarter overlayStarter)
     {
         Time.timeScale = 0;
         this.playerBehaviour = playerBehaviour;
-        this.text = targetText.Replace("\\n", "\n"); ;
+        this.text = computeStringArray(targetText);
         this.overlayStarter = overlayStarter;
         textUI.text = "";
         revealingCoroutine = StartCoroutine("revealText");
+    }
+
+    private string[] computeStringArray(string[] array)
+    {
+        List<string> result = new List<string>(array.Length);
+        for(int x = 0; x<array.Length;x++)
+        {
+            array[x] = array[x].Replace("\\n", "\n");
+            while (array[x].Length > maxLenString)
+            {
+                int splitPos = findSplitPosition(array,x);
+                if (splitPos == -1)
+                {
+                    //no split pos
+                    result.Add(array[x].Substring(0, maxLenString));
+                    array[x] = array[x].Remove(0, maxLenString);
+                }
+                else
+                {
+                    result.Add(array[x].Substring(0, splitPos));
+                    array[x] = array[x].Remove(0, splitPos);
+                }
+            }
+            result.Add(array[x]);
+        }
+        return result.ToArray();
+        
+    }
+
+    private int findSplitPosition(string[] array,int x)
+    {
+        //split into multiple strings
+        int index = maxLenString - 1;
+        while ((array[x])[index] != ' ')
+        {
+            index--;
+            if (index < 1)
+            {
+                //we won't be able to split based on spaces
+                return -1;
+            }
+        }
+        index++;
+        return index;
     }
 
     private void end_dialog()
@@ -54,15 +117,42 @@ public class DialogBorderBehaviour : MonoBehaviour
         this.gameObject.SetActive(false);
     }
 
+    private bool checkForCommands()
+    {
+        if(text[displayIndex][0] != '|') return false;
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int x = 1;x<text[displayIndex].Length;x++)
+        {
+            char next = text[displayIndex][x];
+            if(next == '|')
+            {
+                //finished
+                if (stringBuilder.ToString().Contains("timedramatic"))
+                {
+                    currentTextRevealInterval = dramaticTextRevealInterval;
+                    text[displayIndex] = text[displayIndex].Remove(0, x);
+                    return true;
+                }
+            }
+            stringBuilder.Append(next);
+        }
+        return false;
+    }
+
     private IEnumerator revealText()
     {
+        currentTextRevealInterval = textRevealInterval;
+        checkForCommands();
+        blinkingArrowAnim.SetBool("Blinking", false);
         coroutineRunning = true;
-        for(int x = 0; x<text.Length;x++)
+        textUI.text = "";
+        for(int x = 0; x<text[displayIndex].Length;x++)
         {
-            textUI.text += text[x];
-            yield return new WaitForSecondsRealtime(textRevealInterval);
+            textUI.text += text[displayIndex][x];
+            yield return new WaitForSecondsRealtime(currentTextRevealInterval);
         }
         coroutineRunning = false;
+        blinkingArrowAnim.SetBool("Blinking", true);
        
     }
 }
--- Riflemon/Assets/Scripts/Overlay/OverlayStarter.cs
+++ Riflemon/Assets/Scripts/Overlay/OverlayStarter.cs
@@ -7,12 +7,15 @@ public class OverlayStarter : MonoBehaviour
     public GameObject dialogBorderObj;
     public bool overlayActive;
 
-
-    public void startDialogBorder(PlayerBehaviour player,string targetText)
+    public void startDialogBorder(PlayerBehaviour player,string[] text)
     {
+        if (text == null || text.Length == 0) return;
+        if (text[0] == "" || text[0] == null) return;
         DialogBorderBehaviour dialogBorder = dialogBorderObj.GetComponent<DialogBorderBehaviour>();
         dialogBorderObj.SetActive(true);
-        dialogBorder.start_dialog(player,targetText,this);
+        dialogBorder.start_dialog(player, text, this);
         overlayActive = true;
     }
+
+    
 }
